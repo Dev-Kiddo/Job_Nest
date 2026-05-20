0cb2eb57f9690b61151c922261111ac049251283
Dev-Kiddo <buzzwithprasanth@gmail.com>
2026-05-20 15:39:45 +0530

    debug: node mailer config on production

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -7,58 +7,24 @@ export const sendEmail = async function (toUser: string, emailSubject: string, e
       return;
     }
 
-    // const transport = nodemailer.createTransport({
-    //   service: "gmail",
-
-    //   auth: {
-    //     user: process.env.GMAIL_USER,
-    //     pass: process.env.GMAIL_PASSWORD,
-    //   },
-    //   tls: {
-    //     rejectUnauthorized: false,
-    //   },
-    //   debug: true,
-    //   logger: true,
-    // });
-
-    const transport = nodemailer.createTransporter({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+    const transport = nodemailer.createTransport({
+      service: "gmail",
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASSWORD,
       },
-      family: 4,
-      tls: {
-        rejectUnauthorized: false,
-      },
-      connectionTimeout: 60000, // 60 seconds
-      greetingTimeout: 30000,
-      socketTimeout: 60000,
     });
 
-    console.log(transport);
+    // console.log(transport);
 
-    const emailInfo = await transport.sendMail(
-      {
-        from: process.env.GMAIL_USER,
-        to: toUser,
-        subject: emailSubject,
-        html: emailContent,
-      },
-      (error, info) => {
-        if (error) {
-          console.log("Gmail Error:", error);
-          console.log("Error Code:", error.code);
-          console.log("Error Command:", error.command);
-        } else {
-          console.log("Email sent:", info.response);
-        }
-      },
-    );
+    const emailInfo = await transport.sendMail({
+      from: process.env.GMAIL_USER,
+      to: toUser,
+      subject: emailSubject,
+      html: emailContent,
+    });
 
-    console.log("EMAIL_INFO", emailInfo);
+    // console.log("EMAIL_INFO", emailInfo);
 
     return emailInfo.messageId;
   } catch (error) {
